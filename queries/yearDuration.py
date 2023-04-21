68cd4ab445bea5e9aa4323fb3bc6c4bdc369840d
Gabriel Belisario <gabrielbelisariof@gmail.com>
2023-04-21 13:48:55 -0400

    preprocess and validate arguments yearDuration.py

--- queries/yearDuration.py
+++ queries/yearDuration.py
@@ -1,13 +1,17 @@
 import sys
-import pymongo 
+import pymongo
+
+if len(sys.argv) != 2 or not sys.argv[1].isnumeric():
+    print('''\nRecuerda pasar como argumento el año de las peliculas como:\n\tpython yearDuration.py 2005\n''')
+    exit(1)
 
 client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.jghkftl.mongodb.net/test")
 db = client['disney_db']
 collection_movies = db['movies']
 
-year =  sys.argv[1]
+year = sys.argv[1]
 
-#Dado un año mostrar la película con mayor duración de ese año.
+# Dado un año mostrar la película con mayor duración de ese año.
 q = collection_movies.aggregate([
     {'$match': {'year': year}},
     {'$sort': {'duration': -1}},
