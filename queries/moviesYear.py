ef80d1a17bf7cb0c897ef084404f19142cb7133c
Gabriel Belisario <gabrielbelisariof@gmail.com>
2023-04-21 13:46:30 -0400

    preprocess and validate arguments moviesYear.py

--- queries/moviesYear.py
+++ queries/moviesYear.py
@@ -1,13 +1,17 @@
 import sys
 import pymongo 
 
+if len(sys.argv) != 2 or not sys.argv[1].isnumeric():
+    print('''\nRecuerda pasar como argumento el año de las peliculas como:\n\tpython moviesYear.py 2005\n''')
+    exit(1)
+
 client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.jghkftl.mongodb.net/test")
 db = client['disney_db']
 collection_movies = db['movies']
 
-year =  sys.argv[1]
+year = sys.argv[1]
 
-#Dado un año YYYY mostrar las películas estrenadas ese año
+# Dado un año YYYY mostrar las películas estrenadas ese año
 q = collection_movies.find({
     'year': year
 })
