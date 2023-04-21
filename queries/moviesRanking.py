3c238257c78d64f70bdeca405ac38db64df4e25a
Gabriel Belisario <gabrielbelisariof@gmail.com>
2023-04-21 13:38:20 -0400

    preprocess and validate arguments moviesRanking.py

--- queries/moviesRanking.py
+++ queries/moviesRanking.py
@@ -1,14 +1,27 @@
 import sys
 import pymongo 
 
+if len(sys.argv) != 3:
+    print('''\nRecuerda pasar como argumento la calificacion minima y maxima de la siguiente manera:\n\tpython moviesRanking.py 3.0 5.0\n''')
+    exit(1)
+
+try:
+    min = float(sys.argv[1])
+    max = float(sys.argv[2])
+except:
+    print('''\nRecuerda pasar como argumento la calificacion minima y maxima de la siguiente manera:\n\tpython moviesRanking.py 3.0 5.0\n''')
+    exit(1)
+
+if min > max:
+    tmp = min
+    min = max
+    max = tmp
+
 client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.jghkftl.mongodb.net/test")
 db = client['disney_db']
 collection_movies = db['movies']
 
-min = sys.argv[1]
-max = sys.argv[2]
-
-#Dado un rango (N, M) mostrar las películas que se encuentren en ese ranking según IMDB.
+# Dado un rango (N, M) mostrar las películas que se encuentren en ese ranking según IMDB.
 q = collection_movies.find({
     'imdb_rating': { "$gt" :  float(min), "$lt" : float(max)}
 })
