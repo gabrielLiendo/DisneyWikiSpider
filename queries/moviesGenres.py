58d862b7710455dad9bd95e4e2b8bf52da9a706b
Gabriel Belisario <gabrielbelisariof@gmail.com>
2023-04-21 13:25:48 -0400

    preprocess and validate arguments in moviesGenres.py

--- queries/moviesGenres.py
+++ queries/moviesGenres.py
@@ -1,17 +1,26 @@
 import sys
-import pymongo 
+import pymongo
+
+if len(sys.argv) < 2:
+    print('''\nRecuerda pasar como argumentos los generos de la siguiente manera:\n\tpython moviesGenres.py "Comedia" "Aventura"\n''')
+    exit(1)
 
 client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.jghkftl.mongodb.net/test")
 db = client['disney_db']
 collection_movies = db['movies']
 
-genres =  sys.argv[1:]
+genres = sys.argv[1:]
+for i in range(len(genres)):
+    tmp = ""
+    for token in genres[i].split(" "):
+        tmp += token.capitalize() + " "
+    genres[i] = tmp[:-1]
 
-#Películas que sean de uno o más géneros dados
+# Películas que sean de uno o más géneros dados
 q = collection_movies.find({
     'genres': {"$all": genres}
 })
 
-print("Las películas con géneros", genres, "son:")
+print("Las películas con géneros", " ".join(genres), " son:")
 for movie in q:
     print(movie['title'])
